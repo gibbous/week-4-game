dd1ec30fbb70a5903d78cb9c00df723a81f240c5
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-05-28 00:55:53 -0400

    javascript now shows random number in right div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,61 @@
-//$(document).ready(function(){
+$(document).ready(function(){
 
 var randomNumber;
 var wins = 0;
 var losses = 0;
 var userScore = 0;
-
+var inputArray =[];
+var num;
+var points;
 
 var cats = ["bub", "grumpy", "venus", "henri"]
 
 //Choose random number to be matched
 function getRandomArbitrary(min, max) {
-    var num = Math.floor(Math.random() * (max - min)) + min;
+     num = Math.floor(Math.random() * (max - min)) + min;
     console.log(num);
 }
 //Display random number
 
 
-var randomNumber = getRandomArbitrary(19,120);
-console.log(randomNumber);
+getRandomArbitrary(19,120);
+$('#randomNumber').html(num);
 
 
 
-//create buttons and assign point values to cats
-//for (i = 0; i < cats.length; i++) {
-//		var b = $("<button>");
-//    	b.addClass("cat-button");
-//    	b.attr("id", cats[i])
-//    	b.text(cats[i]);
-//    	b.val(getRandomArbitrary (1,12));
-//    	console.log(b);
-        
-//			$("#buttons").append(b);
-
-//}
+//assign point values to cats
+for (i = 0; i < cats.length; i++) {
+  points= getRandomArbitrary (1,12);
+  var b = $("<button>");
+   // 	b.addClass("cat-button");
+	//b.attr("id", cats[i]);
+  //b.text(cats[i]);
+ 	b.value = (b);
+	console.log(b);
+  
+};
 
 
 //add up points as user clicks
-  $('.cat-button').on('click', function() {
-        console.log('number', this);
-        var number = ($(this).attr('value'));
-            console.log(number);
-           // console.log("resultSpot", resultSpot);
-           // $(resultArray[resultSpot]).html(number);
-          //  inputArray.push(number);
-        });//('#score').append(0);
-	//	});
+  $(".cat-button").on("click", function(){
+      console.log('number', this);
+      var number = ($(this).attr('value'));
+      console.log(number);
+      inputArray.push(number);
+      function getSum(total, num) {
+        return total + Math.round(num);
+       }
+      function myFunction(item) {
+       
+
+      $('#score').html(inputArray.reduce(getSum,0));
+    }
+    });
+
+
 //compare score to starting number
 
 //update wins and losses
 
 //restart game
+});
