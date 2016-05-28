6ddbd7075ef6ea0c02889c74d21dc9f6b71f8ca2
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-05-27 23:46:18 -0400

    adding page styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //$(document).ready(function(){
 
-var randomNumber
+var randomNumber;
 var wins = 0;
 var losses = 0;
 var userScore = 0;
@@ -22,23 +22,29 @@ console.log(randomNumber);
 
 
 //create buttons and assign point values to cats
-for (i = 0; i < cats.length; i++) {
-		var b = $("<button>");
-    	b.addClass("cat-button");
-    	b.attr("data-num", cats[i]);
-    	b.text(cats[i]);
-    	b.val(getRandomArbitrary (1,12));
-    	console.log(b);
-			$("#buttons").append(b);
+//for (i = 0; i < cats.length; i++) {
+//		var b = $("<button>");
+//    	b.addClass("cat-button");
+//    	b.attr("id", cats[i])
+//    	b.text(cats[i]);
+//    	b.val(getRandomArbitrary (1,12));
+//    	console.log(b);
+        
+//			$("#buttons").append(b);
 
-}
+//}
 
 
 //add up points as user clicks
   $('.cat-button').on('click', function() {
-	   
-			$('#score').append(0);
-		});
+        console.log('number', this);
+        var number = ($(this).attr('value'));
+            console.log(number);
+           // console.log("resultSpot", resultSpot);
+           // $(resultArray[resultSpot]).html(number);
+          //  inputArray.push(number);
+        });//('#score').append(0);
+	//	});
 //compare score to starting number
 
 //update wins and losses
